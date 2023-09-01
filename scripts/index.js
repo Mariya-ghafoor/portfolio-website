3ee09cecb9a560734599321ffa8d7335fcf49048
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-09-01 22:14:35 +1000

    fixes hamburger menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function menuToggleHandler() {
   console.log("i m in my function");
-  var x = document.getElementById("navbar-links");
+  let x = document.getElementById("navbar-links");
   if (x.style.display === "flex") {
     x.style.display = "none";
   } else {
@@ -13,8 +13,8 @@ function menuToggleHandler() {
 //if screen size increases while on hamburger menu we need to close it
 function screen_resize() {
   //   // var h = parseInt(window.innerHeight);
-  var w = parseInt(window.innerWidth);
-  var x = document.getElementById("navbar-links");
+  let w = parseInt(window.innerWidth);
+  let x = document.getElementById("navbar-links");
   console.log("screen resize");
 
   if (w > 641) {
@@ -27,60 +27,6 @@ function screen_resize() {
   }
 }
 
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function () {
-  scrollFunction();
-};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-// }
-
-// Run the script only when DOM is finished loading
-
-window.addEventListener("DOMContentLoaded", () => {
-  const logo = document.getElementById("logo");
-
-  // Add a :) to the logo when mouse enters
-  logo.addEventListener(
-    "mouseenter",
-    () => (logo.innerHTML = " Mariya Abdul Ghafoor :)")
-  );
-
-  // Remove the :) when mouse leaves
-  logo.addEventListener(
-    "mouseleave",
-    () => (logo.innerHTML = " Mariya Abdul Ghafoor")
-  );
-});
-
-//******Text appear slowly when page loads  */
-// window.onload = function () {
-//   let currentTextClass = "";
-//   //This will give the id of current navlink
-//   const currentHash = window.location.hash;
-
-//   //If user has not yet clicked any link this will be empty then we can assign it 'container__text--home' class
-//   if (currentHash == "") {
-//     currentTextClass = "container__text--home";
-//   } else {
-//     console.log(currentHash.slice(1));
-//     currentTextClass = "container__text--" + currentHash.slice(1);
-//   }
-//   console.log(document.getElementsByClassName(currentTextClass)[0]);
-//   document
-//     .getElementsByClassName(currentTextClass)[0]
-//     .classList.add("appear-text");
-//   // document
-//   //   .getElementsByClassName("container__text--landing")[0]
-//   //   .classList.add("appear-text");
-// };
-
 //*****Highlight the nav link of current page *****/
 let lastActiveLinkClassList = 0;
 const navLinkHandler = (event) => {
@@ -92,4 +38,8 @@ const navLinkHandler = (event) => {
   //Add the active class to the current clicked link
   event.target.classList.add("active");
   lastActiveLinkClassList = event.target.classList;
+
+  // Close the hamburger menu after a link is clicked only if we are on small screen size
+  let w = parseInt(window.innerWidth);
+  if (w <= 641) menuToggleHandler();
 };
